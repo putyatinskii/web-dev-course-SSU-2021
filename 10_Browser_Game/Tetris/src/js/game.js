04e2160b44a6e70ba51a3c93572b41ebc9a59fcc
dputiatinskii <dputiatinskii@neoflex.ru>
2022-04-01 13:40:40 +0300

    added shape rotate

--- 10_Browser_Game/Tetris/src/js/game.js
+++ 10_Browser_Game/Tetris/src/js/game.js
@@ -61,6 +61,40 @@ export default class Game {
         
     }
 
+    rotatePiece() {
+        this.rotateBlocks()
+
+        if (this.hasCollision()) {
+            this.rotateBlocks(false);
+        }
+    }
+
+    rotateBlocks(clockwise = true) {
+        const blocks = this.activePiece.blocks;
+        const length = blocks.length;
+        const x = Math.floor(length / 2);
+        const y = length - 1;
+
+        for (let i = 0; i < x; i++) {
+            for (let j = 0; j < y - i; j++) {
+                const temp = blocks[i][j];
+
+                if (clockwise) {
+                    blocks[i][j] = blocks[y - j][i];
+                    blocks[y - j][i] = blocks[y - i][y - j];
+                    blocks[y - i][y - j] = blocks[j][y - i];
+                    blocks[j][y - i] = temp;
+                } else {
+                    blocks[i][j] = blocks[j][y - i];
+                    blocks[j][y - i] = blocks[y - i][y - j];
+                    blocks[y - i][y - j] = blocks[y - j][i];
+                    blocks[y - j][i] = temp;
+                }
+            }
+            
+        }  
+    }
+
     hasCollision() {
         const {y: pieceY, x: pieceX, blocks} = this.activePiece;
         
