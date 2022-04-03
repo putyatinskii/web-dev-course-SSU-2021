e168f0b3c6f2b51e0769825b22888df6e6c11252
dputiatinskii <dputiatinskii@neoflex.ru>
2022-04-03 13:29:22 +0300

    добавление таймера

--- 10_Browser_Game/Tetris/src/js/app.js
+++ 10_Browser_Game/Tetris/src/js/app.js
@@ -1,34 +1,14 @@
 import '../scss/app.scss';
 import Game from './game';
 import View from './view';
+import Controller from './controller';
 
 const root = document.querySelector('#root');
 
 const game = new Game();
 const view = new View(root, 480, 640, 20, 10);
+const controller = new Controller(game, view);
 
 window.game = game;
 window.view = view;
-
-document.addEventListener('keydown', event => {
-   switch(event.keyCode) {
-        case 37:
-            game.movePieceLeft();
-            view.render(game.getState());
-            break;
-        case 38:
-            game.rotatePiece();
-            view.render(game.getState());
-            break;
-        case 39:
-            game.movePieceRight();
-            view.render(game.getState());
-            break;
-        case 40:
-            game.movePieceDown();
-            view.render(game.getState());
-            break;
-   } 
-});
-
-view.render(game.getState());
+window.controller = controller;
